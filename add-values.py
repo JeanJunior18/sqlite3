d0a54e47c2e633f8d3c2899bfcfde62d7948306a
JeanJunior18 <54405723+JeanJunior18@users.noreply.github.com>
2019-11-25 21:21:48 -0300

    Add files via upload

---
+++ add-values.py
@@ -0,0 +1,29 @@
+import sqlite3 as sq
+
+# Conecta ao banco de dados e cria o cursor
+con = sq.connect('agenda.db')
+cursor = con.cursor()
+
+# Comando para inserir dados nas tabelas individualmente
+cursor.execute("""
+    insert into clientes(nome, cpf)
+    values ("Jean Charles", "616.803.353-08")
+""")
+# Adicionar vários dados de uma vez
+cadastros = [
+    # Lebrando que também podem ser variaveis incluidas através do input na lista
+    ("Junior Paiva", "111.111.111-11"),
+    ("Jose Lucas", "222.222.222-22"),
+    ("Leandro", "333.333.333-33")
+]
+
+cursor.executemany("""
+    insert into clientes (nome, cpf)
+    values(?,?)
+""", cadastros) # Aqui é colocado a lista que oculpara as interrogações
+
+
+
+# Salva os dados no banco de dados e fecha a conexão
+con.commit()
+con.close()
